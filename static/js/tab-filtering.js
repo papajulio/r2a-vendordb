572588828ae322815cbe5821bc9362976e7a9cd4
figarocorso <figarocorso@gmail.com>
2018-06-10 18:55:35 +0200

    Add filtering feature
    - Every choice narrows the number of vendors
    - Every step choices list is narrowed by the current vendors list

--- static/js/tab-filtering.js
+++ static/js/tab-filtering.js
@@ -1,38 +1,34 @@
-function tabWasClicked(event, tab) {
-    var loaderFunction = {
-        0: loadStep0,
-        1: loadStep1,
-        2: loadStep2,
-        3: loadStep3
-    };
+var loaderFunction = {
+    0: loadStep0,
+    1: loadStep1,
+    2: loadStep2,
+    3: loadStep3,
+    4: loadVendors
+};
 
+function tabWasActivated(event, tab) {
     var selectedStep = tab.tab.attr("data-step");
     var targetElement = tab.panel.find(".list-container");
     var selectedItemTitle = tab.tab.find(".filter-selected").attr("data-filter");
-
-    showSpinner(targetElement);
-
-    if (loaderFunction[selectedStep]) {
-        loaderFunction[selectedStep](targetElement, selectedItemTitle);
-    }
-    addClickListenerToChoicesRow();
-}
-
-function showSpinner(element) {
-    element.html("<div class='center'><i class='fa fa-spin fa-refresh fa-3x'></i></div>");
-}
-
-function addClickListenerToChoicesRow() {
-    $(".choices-row").off('click').click(choiceWasClicked);
+    loadStepContent(selectedStep, targetElement, selectedItemTitle);
 }
 
 function choiceWasClicked(e) {
     e.preventDefault();
     var step = $(this).attr("data-step");
+    var choiceId = $(this).attr("data-choice-id");
     var title = $(this).find("h3").text();
     $(this).addClass("choice-selected");
     setFilteringStep(step, title);
-    updateResult(step, title);
+    stepWasChanged(step, choiceId);
+    activateNextStep(step);
+    scrollToTableHeader();
+}
+
+function scrollToTableHeader() {
+    $('html, body').animate({
+        scrollTop: $("#postHeader").offset().top
+    }, 500);
 }
 
 function addClickListenerToClearFilter() {
@@ -45,7 +41,7 @@ function clearFilteringStep(e) {
     var tab = $("#vendorFilters").find("li[data-step=" + step + "]");
     var accordionTab = $(".r-tabs-accordion-title.r-tabs-state-active");
 
-    updateResult(step, "");
+    stepWasChanged(step, "");
 
     $(tab.find("a").attr("href")).find(".choice-selected").each(function (){
         $(this).removeClass("choice-selected");
@@ -91,24 +87,44 @@ function setFilteringStep(step, filter) {
 
     tab.find(".clear-filter-icon").removeClass("hidden");
     accordionTab.find(".clear-filter-icon").removeClass("hidden");
-
-    activateNextStep(step);
 }
 
 function activateNextStep(step) {
-    $("#vendorFilters").responsiveTabs("activate", parseInt(step) + 1)
+    var nextStep = parseInt(step) + 1;
+    $("#vendorFilters").responsiveTabs("activate", nextStep);
+}
+
+function loadStepContent(selectedStep, targetElement, selectedItemTitle) {
+    showSpinner(targetElement);
+    if (loaderFunction[selectedStep]) {
+        loaderFunction[selectedStep](targetElement, selectedItemTitle);
+    }
+    addClickListenerToChoicesRow();
+}
+
+function showSpinner(element) {
+    element.html("<div class='center'><i class='fa fa-spin fa-refresh fa-3x'></i></div>");
+}
+
+function addClickListenerToChoicesRow() {
+    $(".choices-row").off('click').click(choiceWasClicked);
 }
 
 $(document).ready(function() {
     $("#vendorFilters").responsiveTabs({
-        activate: tabWasClicked,
+        activate: tabWasActivated,
         startCollapsed: 'accordion'
     });
 
     addClickListenerToChoicesRow();
     addClickListenerToClearFilter();
+    refreshUI();
 });
 
-function updateResult(step, title) {
-    $(".vendors-number").html(parseInt(Math.random() * 100));
+function refreshUI() {
+    showNumberOfVendors();
+}
+
+function showNumberOfVendors() {
+    $(".vendors-number").html(": " + parseInt(vendorsToShow.length));
 }
--- static/js/tabs-data-loader.js
+++ static/js/tabs-data-loader.js
@@ -1,47 +1,274 @@
+// Global variables containing JSON information
+var jsonLoaded = false;
+var objectives;
+var personas;
+var technologies;
+var vendors;
+
+// Global variables containing filtering
+var filters = {
+    0: "",
+    1: "",
+    2: "",
+    3: ""
+}
+
+var vendorsToShow = getVendorsToShow();
+
+$.getJSON("db.json").done(function(json) {
+    objectives = json.objectives;
+    personas = json.personas;
+    technologies = json.technologies;
+    vendors = json.vendors;
+
+    jsonLoaded = true;
+});
+
+function stepWasChanged(step, choiceId) {
+    step = parseInt(step);
+    filters[step] = choiceId;
+    vendorsToShow = getVendorsToShow();
+    showNumberOfVendors();
+}
+
 function loadStep0(targetElement, selectedItemTitle) {
-    loadDemoStep(targetElement, 0, selectedItemTitle);
+    var personaIdsToShow = getPersonaIdsFromVendors();
+    targetElement.html("");
+    var step = 0;
+    for (var personaId of personaIdsToShow) {
+        var rowSelectedClass = "";
+        if (personas[personaId]["n"] == selectedItemTitle) {
+            rowSelectedClass = " choice-selected ";
+        }
+        targetElement.append($('\
+            <div class="main-row row sqs-row">\
+                <div class="col sqs-col-12 span-12">\
+                    <div class="row sqs-row">\
+                        <div class="choices-row ' + rowSelectedClass + 'col sqs-col-12 span-12" data-step="' + step + '" data-choice-id="' + personaId + '">\
+                            <h3>' + personas[personaId]["n"] + '</h3>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>'));
+    }
+}
+
+function getPersonaIdsFromVendors() {
+    vendorsToShow = getVendorsToShow();
+
+    var objectiveIds = [];
+    for (var vendorId in vendorsToShow) {
+        objectiveIds = mergeArrays(objectiveIds, vendorsToShow[vendorId].o);
+    }
+
+    var result = [];
+    for (var personaId in personas) {
+        for (var personaObjective of personas[personaId].o) {
+            if (objectiveIds.indexOf(personaObjective) != -1) {
+                result = mergeArrays(result, [personaId]);
+                break;
+            }
+        }
+    }
+    return result.sort();
 }
 
 function loadStep1(targetElement, selectedItemTitle) {
-    loadDemoStep(targetElement, 1, selectedItemTitle);
-}
 
-function loadStep2(targetElement, selectedItemTitle) {
-    loadDemoStep(targetElement, 2, selectedItemTitle);
-}
-
-function loadStep3(targetElement, selectedItemTitle) {
-    loadDemoStep(targetElement, 3, selectedItemTitle);
-}
-
-function loadDemoStep(targetElement, step, selectedItemTitle) {
+    var objectiveIdsToShow = getObjectiveIdsFromVendors();
     targetElement.html("");
-    var titleElements = [
-        "Dapper Drake",
-        "Hardy Heron",
-        "Lucid Lynx",
-        "Precise Pangolin",
-        "Trusty Tahr",
-        "Xenial Xerus",
-        "Bionic Beaver"
-    ];
-    titleElements = shuffle(titleElements);
-    for (var title of titleElements) {
+
+    var step = 1;
+    for (var objectiveId of objectiveIdsToShow) {
         var rowSelectedClass = "";
-        if (title == selectedItemTitle) {
+        if (objectives[objectiveId]["n"] == selectedItemTitle) {
             rowSelectedClass = " choice-selected ";
         }
         targetElement.append($('\
             <div class="main-row row sqs-row objective" data-objective-id="0" data-categories="vaca">\
                 <div class="col sqs-col-12 span-12">\
                     <div class="row sqs-row">\
-                        <div class="choices-row ' + rowSelectedClass + 'col sqs-col-12 span-12" data-step="' + step + '">\
-                            <h3>' + title + '</h3>\
-                            <ul class="tags"><li>One of those objectives</li></ul>\
-                            <p class="use_case clear">Manage queries on new regulations with a chatbot</p>\
+                        <div class="choices-row ' + rowSelectedClass + 'col sqs-col-12 span-12" data-step="' + step + '" data-choice-id="' + objectiveId + '">\
+                            <h3>' + objectives[objectiveId]["n"] + '</h3>\
                         </div>\
                     </div>\
                 </div>\
             </div>'));
     }
+}
+
+function getObjectiveIdsFromVendors() {
+    vendorsToShow = getVendorsToShow();
+
+    var objectiveIds = [];
+    for (var vendorId in vendorsToShow) {
+        objectiveIds = mergeArrays(objectiveIds, vendorsToShow[vendorId].o);
+    }
+    return objectiveIds.sort();
+}
+
+function loadStep2(targetElement, selectedItemTitle) {
+    var technologyIdsToShow = getTechonologyIdsFromVendors();
+    targetElement.html("");
+
+    var step = 2;
+    for (var technologyId of technologyIdsToShow) {
+        var rowSelectedClass = "";
+        if (technologies[technologyId]["n"] == selectedItemTitle) {
+            rowSelectedClass = " choice-selected ";
+        }
+        targetElement.append($('\
+            <div class="main-row row sqs-row objective" data-objective-id="0" data-categories="vaca">\
+                <div class="col sqs-col-12 span-12">\
+                    <div class="row sqs-row">\
+                        <div class="choices-row ' + rowSelectedClass + 'col sqs-col-12 span-12" data-step="' + step + '" data-choice-id="' + technologyId + '">\
+                            <h3>' + technologies[technologyId]["n"] + '</h3>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>'));
+    }
+}
+
+function getTechonologyIdsFromVendors() {
+    vendorsToShow = getVendorsToShow();
+
+    var technologyIds = [];
+    for (var vendorId in vendorsToShow) {
+        technologyIds = mergeArrays(technologyIds, vendorsToShow[vendorId].t);
+    }
+    return technologyIds.sort();
+}
+
+function loadStep3(targetElement, selectedItemTitle) {
+    var locations = getLocations();
+    targetElement.html("");
+
+    var step = 3;
+    for (var locationId in locations) {
+        var rowSelectedClass = "";
+        if (locations[locationId] == selectedItemTitle) {
+            rowSelectedClass = " choice-selected ";
+        }
+        targetElement.append($('\
+            <div class="main-row row sqs-row location" data-location-id="0" data-categories="vaca">\
+                <div class="col sqs-col-12 span-12">\
+                    <div class="row sqs-row">\
+                        <div class="choices-row ' + rowSelectedClass + 'col sqs-col-12 span-12" data-step="' + step + '" data-choice-id="' + locations[locationId] + '">\
+                            <h3>' + locations[locationId] + '</h3>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>'));
+    }
+}
+
+function getLocations() {
+    vendorsToShow = getVendorsToShow();
+    var locations = [];
+    for (var vendorId in vendorsToShow) {
+        locations = mergeArrays(locations, vendorsToShow[vendorId].g);
+    }
+    locations.sort();
+    if (locations.indexOf("Global") != -1) {
+        locations.splice(locations.indexOf("Global"), 1);
+        locations.splice(0, 0, "Global");
+    }
+    return locations;
+}
+
+function loadVendors(targetElement, selectedItemTitle) {
+    var step = 4;
+
+    targetElement.html("");
+
+    vendorsToShow = getVendorsToShow();
+
+    for (var vendorId in getVendorsToShow()) {
+        var rowSelectedClass = "";
+        if (vendorsToShow[vendorId]["n"] == selectedItemTitle) {
+            rowSelectedClass = " choice-selected ";
+        }
+        targetElement.append($('\
+            <div class="main-row row sqs-row vendor" data-vendor-id="0" data-categories="vaca">\
+                <div class="col sqs-col-12 span-12">\
+                    <div class="row sqs-row">\
+                        <div class="choices-row ' + rowSelectedClass + 'col sqs-col-12 span-12" data-step="' + step + '">\
+                            <h3>' + vendorsToShow[vendorId]["n"] + '</h3>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>'));
+    }
+}
+
+function getVendorsToShow() {
+    var result = [];
+    for (var vendorId in vendors) {
+        if (shouldShowVendor(vendors[vendorId])) {
+            result.push(vendors[vendorId]);
+        }
+    }
+    return result;
+}
+
+function shouldShowVendor(vendor) {
+    return step0ShouldShowVendor(vendor)
+        && step1ShouldShowVendor(vendor)
+        && step2ShouldShowVendor(vendor)
+        && step3ShouldShowVendor(vendor);
+}
+
+function step0ShouldShowVendor(vendor) {
+    var stepFilter = filters[0];
+    if (stepFilter == "") {
+        return true;
+    }
+
+    for (var objectiveId of personas[parseInt(stepFilter)].o) {
+        if (vendor.o.indexOf(parseInt(objectiveId)) != -1) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function step1ShouldShowVendor(vendor) {
+    var stepFilter = filters[1];
+    if (stepFilter == "") {
+        return true;
+    }
+
+    if (vendor.o.indexOf(parseInt(stepFilter)) != -1) {
+        return true;
+    }
+
+    return false;
+}
+
+function step2ShouldShowVendor(vendor) {
+    var stepFilter = filters[2];
+    if (stepFilter == "") {
+        return true;
+    }
+
+    if (vendor.t.indexOf(parseInt(stepFilter)) != -1) {
+        return true;
+    }
+
+    return false;
+}
+
+function step3ShouldShowVendor(vendor) {
+    var stepFilter = filters[3];
+    if (stepFilter == "") {
+        return true;
+    }
+
+    if (vendor.g.indexOf(stepFilter) != -1) {
+        return true;
+    }
+
+    return false;
 }
