6c85672d3502924f2cf925129bee11e68f1cbe41
figarocorso <figarocorso@gmail.com>
2018-07-08 17:45:37 +0200

    Redesign filtering UI
    Remove tabs using left filtering bar instead

---
+++ static/js/filtering-ui.js
@@ -0,0 +1,102 @@
+function vendorWasClicked(e) {
+    e.preventDefault();
+    var vendorId = $(this).attr("data-vendor-id");
+    console.log("Vendor was clicked: " + vendorId);
+}
+
+function filterWasClicked(e) {
+    e.preventDefault();
+    toggleFilterBox($(this).parent());
+    refreshUI();
+}
+
+function toggleFilterBox(filterBoxElement) {
+    filterBoxElement.find(".choices-box").toggleClass("hidden", 1000);
+    filterBoxElement.find("i").toggleClass("fa-angle-down").toggleClass("fa-angle-right");
+}
+
+function choiceWasClicked(e) {
+    e.preventDefault();
+
+    var title = $(this).find(".title").text();
+    var step = $(this).attr("data-step");
+    $(".selected-filter-" + step).find("span").html(title);
+    $(".selected-filter-" + step).removeClass("hidden");
+
+    toggleFilterBox($(this).parent().parent());
+
+    var choiceId = $(this).attr("data-choice-id");
+    setFilteringStep(step, choiceId);
+    scrollToTableHeader();
+    refreshUI();
+}
+
+function clearFilterWasClicked(e) {
+    e.preventDefault();
+    var step = $(this).attr("data-step");
+    filters[step] = "";
+    $(this).toggleClass("hidden");
+    toggleFilterBox($(this).parent());
+    refreshUI();
+}
+
+function scrollToTableHeader() {
+    $('html, body').animate({
+        scrollTop: $("#page").offset().top
+    }, 500);
+}
+
+function setFilteringStep(step, filter) {
+    if (filters[step] == filter) {
+        $(".selected-filter-" + step).addClass("hidden");
+        filters[step] = "";
+    } else {
+        filters[step] = filter;
+    }
+}
+
+function showSpinner(element) {
+    element.html("<div class='center'><i class='fa fa-spin fa-refresh fa-3x'></i></div>");
+}
+
+function addClickListenerToFilters() {
+    $(".filter-title-box").off('click').click(filterWasClicked);
+}
+
+function addClickListenerToChoices() {
+    $(".choice").off('click').click(choiceWasClicked);
+}
+
+function addClickListenerToFiltersClean() {
+    $(".selected-filter").off('click').click(clearFilterWasClicked);
+}
+
+function addClickListenerToVendorRows() {
+    $(".vendor-row").off('click').click(vendorWasClicked);
+}
+
+$(document).ready(function() {
+    refreshUI();
+    addClickListenerToFilters();
+    addClickListenerToChoices();
+    addClickListenerToVendorRows();
+    addClickListenerToFiltersClean();
+});
+
+function refreshUI() {
+    loadStep0();
+    loadStep1();
+    loadStep2();
+    loadStep3();
+    loadVendors();
+
+    showNumberOfVendors();
+
+    addClickListenerToChoices();
+    addClickListenerToFiltersClean();
+    addClickListenerToVendorRows();
+}
+
+function showNumberOfVendors() {
+    $(".vendors-number").html(": " + parseInt(vendorsToShow.length));
+}
