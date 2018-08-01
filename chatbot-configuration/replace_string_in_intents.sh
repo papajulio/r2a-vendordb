3d3d6b8a0f8cd4cc7df7a266dbc5a6dca862559e
Julio J <papajulio21@gmail.com>
2018-08-01 15:36:32 +0200

    Move entities to his own subfolder

--- chatbot-configuration/replace_string_in_intents.sh
+++ chatbot-configuration/replace_string_in_intents.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-FILES=data/*
+FILES=data/*.json
+for FILE in $FILES
+do
+    sed -i "s/$1/$2/g" $FILE
+done
+
+FILES=data/entities/*
 for FILE in $FILES
 do
     sed -i "s/$1/$2/g" $FILE
